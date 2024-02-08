0139423e2d62650a8f200ae0f38de0139d236459
d0x45 <96327554+D0x45@users.noreply.github.com>
2024-02-08 20:37:21 +0330

    some changes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function App() {
     const [counter, setCounter] = useState(0);
     return h(
         'div', { name: this.constructor.name },
+        h('input', { type: 'text', class: 'bg-gray-50 border border-gray-300' }),
         h('button', {
             type: 'button',
             class: 'rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50',
