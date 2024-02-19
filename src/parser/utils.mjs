96a81ebd79f0038a1e4f10a5bf8f5b6b00f6cfdc
D0x45 <96327554+D0x45@users.noreply.github.com>
2024-02-19 15:17:05 +0330

    a little housekeeping

--- src/parser/utils.mjs
+++ src/parser/utils.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import ExcelJS from 'exceljs';
 
+/** empty cell string indicating null or non-existing value */
+const defaultEmptyCell = '-';
+
 const daysOfWeek = [
     'شنبه',
     'یکشنبه',
@@ -196,26 +199,97 @@ function timeEq(a, b) {
         && (a.minute === b.minute);
 }
 
+function timeToStr(hour, minute, forceMinute = false) {
+    let s = !forceMinute ? hour : padLeft(hour || 0);
+    if (minute || forceMinute)
+        s += ':' + padLeft(minute || 0);
+    return s;
+}
+
 /** @type {import('./types').ClassInfoValueToStr<'sessions'>} */
 function defaultSessionToStr() {
-    const m0 = this.starts.minute ? `:${padLeft(this.starts.minute)}` : '';
-    const m1 = this.ends.minute   ? `:${padLeft(this.ends.minute)}`   : '';
-    let tmp = `${dayToStr(this.day)} ${this.starts.hour}${m0} تا ${this.ends.hour}${m1} `;
-    if (this.place) tmp += `(${this.place}) `;
-    if (this.dates) tmp += this.dates === 'odd' ? '[فرد]' : '[زوج]';
+    const t0 = timeToStr(this.starts.hour, this.starts.minute, false),
+          t1 = timeToStr(this.ends.hour, this.ends.minute, false),
+          d = dayToStr(this.day);
+    let tmp = `${d} ${t0} تا ${t1}`;
+    // this will be shown somewhere else as it takes a lot of space
+    // if (this.place) tmp += `(${this.place}) `;
+    if (this.dates) tmp += this.dates === 'odd' ? ' [فرد]' : ' [زوج]';
     return tmp.trim();
-};
+}
 
 /** @type {import('./types').ClassInfoValueToStr<'exams'>} */
 function defaultExamToStr() {
     return `${this.year}/${padLeft(this.month)}/${padLeft(this.day)} ${padLeft(this.hour)}:${padLeft(this.minute)}`;
 }
 
-/** empty cell string indicating null or non-existing value */
-const defaultEmptyCell = '-';
+function clamp(a, min, max) {
+    return Math.min(Math.max(a, min), max);
+}
+
+/**
+ * @template T
+ * @param {T[]} p0
+ * @param {T[]} p1
+ * @returns {Array<{a: T, b: T}>}
+ */
+function permutations(p0, p1) {
+    return p0.flatMap(a => p1.map( b => ({a, b}) ));
+}
+
+/**
+ * @param {number} start
+ * @param {number} stop
+ * @param {number} step
+ * @returns {number[]}
+ */
+function rangeArray(start, stop, step = 1) {
+    return Array.from(
+        { length: (stop - start) / step + 1 },
+        (_, index) => (start + index * step)
+    );
+}
+
+function makeTimeSpans(startHour, endHour, minuteSpan) {
+    return permutations(
+        rangeArray(
+            clamp(startHour, 0, 23),
+            clamp(endHour, 0, 23)
+        ),
+        rangeArray(0, 59, clamp(minuteSpan, 1, 60))
+    );
+}
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+function gcd(a, b) {
+    if (a === 0) return b;
+    return gcd(b % a, a);
+}
+
+/**
+ * @template [T=number]
+ * @param {T} a
+ * @param {T} b
+ * @param {T} c
+ * @param {T} d
+ * @param {undefined|boolean} inclusive [08:00, 09:00] and [09:00, 10:00] are inclusive overlapping
+ * @returns {boolean}
+ * @see https://stackoverflow.com/questions/36011227/javascript-check-if-time-ranges-overlap
+ */
+function rangesOverlap(a, b, c, d, inclusive = undefined) {
+    return inclusive
+        ? (b >= c && a <= d)
+        : (b >  c && a <  d);
+}
 
 export {
     classInfoKeyTitles,
+    defaultEmptyCell,
+    daysOfWeek,
     sanitizeFarsi,
     dayFromStr,
     dayToStr,
@@ -224,5 +298,11 @@ export {
     parseXLSX,
     defaultExamToStr,
     defaultSessionToStr,
-    defaultEmptyCell,
+    clamp,
+    timeToStr,
+    makeTimeSpans,
+    rangeArray,
+    permutations,
+    gcd,
+    rangesOverlap,
 };
